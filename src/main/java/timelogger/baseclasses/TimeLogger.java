59f4c615920feddc00698d79888d06e637a19301
Bence Bors <bbors@precognox.com>
2017-10-25 15:04:46 +0200

    Lombok and javadoc refactoring of TimeLogger class

--- src/main/java/timelogger/baseclasses/TimeLogger.java
+++ src/main/java/timelogger/baseclasses/TimeLogger.java
@@ -2,13 +2,13 @@ package timelogger.baseclasses;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
+import lombok.Data;
 
 /**
  *
  * @author bbi93
  */
-@Getter
+@Data
 public class TimeLogger {
 
 	private List<WorkMonth> months = new ArrayList<>();
@@ -19,6 +19,11 @@ public class TimeLogger {
 		}
 	}
 
+	/**
+	 *
+	 * @param wm Workmonth to check.
+	 * @return boolean Return treu, if workmonth list not contains workmonth with same value date field.
+	 */
 	public boolean isNewMonth(WorkMonth wm) {
 		return months.stream().filter(month -> month.getDate().equals(wm.getDate())).count() == 0;
 	}
